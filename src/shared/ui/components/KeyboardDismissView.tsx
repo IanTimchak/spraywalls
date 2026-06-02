80aa4aa288ade123fd26df5b1e16fc7519104022
Ian Timchak <60276491+IanTimchak@users.noreply.github.com>
2026-06-01 23:08:58 -0400

    bugfix: keyboard not working in safari

--- src/shared/ui/components/KeyboardDismissView.tsx
+++ src/shared/ui/components/KeyboardDismissView.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from 'react';
-import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { Keyboard, Platform, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
 
 export default function KeyboardDismissView({ children }: PropsWithChildren) {
+    if (Platform.OS === 'web') {
+        return <View style={styles.container}>{children}</View>;
+    }
+
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View style={styles.container}>{children}</View>
